e7c5c87653d6d2ff53d094fea0fd87c709021e14
Harlan Wood <code@harlantwood.net>
2024-04-15 00:30:32 -1000

    Push openai TTS down into ai-convo

--- src/routes/api/question/audio/+server.js
+++ src/routes/api/question/audio/+server.js
@@ -1,25 +1,14 @@
-import OpenAI from 'openai'
-import { Buffer } from 'buffer/'
-
+import { tts } from 'ai-convo'
 import { OPENAI_API_KEY } from '$env/static/private'
 
 export async function POST({ request }) {
   const { text } = await request.json()
-
-  const openai = new OpenAI({ apiKey: OPENAI_API_KEY })
-
-  const mp3 = await openai.audio.speech.create({
-    model: 'tts-1',
-    voice: 'alloy',
-    input: text,
-  })
-
-  const buffer = Buffer.from(await mp3.arrayBuffer())
-
-  return new Response(buffer, {
-    status: 200,
-    headers: {
-      'Content-Type': 'audio/mpeg',
-    },
-  })
+  const response = await tts.server.openai.response(
+    text,
+    {
+      model: 'tts-1',
+      voice: 'alloy',
+      apiKey: OPENAI_API_KEY
+    })
+  return response
 }
