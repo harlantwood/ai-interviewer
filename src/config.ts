6a45ee9ceecda843f1d044f3778a1edd8480d184
Harlan Wood <code@harlantwood.net>
2024-05-24 15:59:14 -1000

    Update content

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const WebsiteName: string = 'SaaS Starter'
-export const WebsiteBaseUrl: string = 'https://saasstarter.work'
+export const WebsiteName: string = 'Intraviewer'
+export const WebsiteBaseUrl: string = 'https://intraviewer.ai'
 export const WebsiteDescription: string =
-  'Open source, fast, and free to host SaaS template. Built with SvelteKit, Supabase, Stripe, Tailwind, DaisyUI, and Postgres'
+  'Interview yourself on any topic to generate content for articles, courses, whatever you want!'
